3b44b10c702702ae8a165fcb6b6c14745e18c29c
Elizaveta <marsela5678@gmail.com>
2023-05-31 23:39:18 +0200

    added second and third lines

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 int main()
 {
-	sdt::cout<<"Hello, world!"<<sdt::endl;
+	std::cout<<"Hello, world!"<<std::endl;
+	std::cout<<"The second line"<<std::endl;
+	std::cout<<"The third line"<<std::endl; 
 	return 0;
 
 }	
